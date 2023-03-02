a4234545e3d7c332e9601a721110a007908dc3e3
Thalis Mateus Barcellos Macedo <100248956+Alcaanon@users.noreply.github.com>
2023-03-02 19:22:57 -0300

    Token - CREATE MANUAL

--- dist/token/token.controller.js
+++ dist/token/token.controller.js
@@ -8,24 +8,38 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TokenController = void 0;
 const common_1 = require("@nestjs/common");
-const auth_service_1 = require("../auth/auth.service");
 const prisma_service_1 = require("../prisma/prisma.service");
-const jwt_1 = require("@nestjs/jwt");
+const bcrypt = require("bcrypt");
 let TokenController = class TokenController {
-    constructor(prisma, authService, jwtService) {
+    constructor(prisma) {
         this.prisma = prisma;
-        this.authService = authService;
-        this.jwtService = jwtService;
+    }
+    async create(data) {
+        const token = await this.prisma.getClient().token.create({
+            data: {
+                email: data.email,
+                hash: bcrypt.hashSync(data.hash, 8),
+            },
+        });
+        return token;
     }
 };
+__decorate([
+    (0, common_1.Post)(),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], TokenController.prototype, "create", null);
 TokenController = __decorate([
     (0, common_1.Controller)('token'),
-    __metadata("design:paramtypes", [prisma_service_1.PrismaService,
-        auth_service_1.AuthService,
-        jwt_1.JwtService])
+    __metadata("design:paramtypes", [prisma_service_1.PrismaService])
 ], TokenController);
 exports.TokenController = TokenController;
 //# sourceMappingURL=token.controller.js.map
--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,82 +1,22 @@
-import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
-import { Token, Usuario } from '@prisma/client';
-import { AuthService } from 'src/auth/auth.service';
+import { Controller, Post, Body } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Controller('token')
 export class TokenController {
   constructor(
     private readonly prisma: PrismaService,
-    private readonly authService: AuthService,
-    private readonly jwtService: JwtService,
   ) {}
 
-
-  // @Post()
-  // async create(@Body() data: { userId: string }): Promise<Token> {
-  //   const token = await this.prisma.token.create({
-  //     data: {
-  //       userId: data.userId,
-  //       token: await this.authService.generateAccessToken(data.userId),
-  //     },
-  //   });
-  //   return token;
-  // }
-
-  // @Post('/save')
-  // async saveToken(token: Token, usuario: Usuario): Promise<Token> {
-  //   const existingToken = await this.prisma.getClient().token.findUnique({
-  //     where: { email: usuario.email },
-  //   });
-
-  //   if (existingToken) {
-  //     await this.prisma.getClient().token.update({
-  //       where: { email: usuario.email },
-  //       data: token,
-  //     });
-  //     return token;
-  //   } else {
-  //     const createdToken = await this.prisma.getClient().token.create({
-  //       data: {
-  //         ...token,
-  //         id: { connect: { email: usuario.email } },
-  //       },
-  //     });
-  //     return createdToken;
-  //   }
-  // }
-
-  // @Post('/refresh')
-  // async refreshToken(@Body() refreshToken: string): Promise<Token> {
-  //   const token = await this.authService.verifyRefreshToken(refreshToken);
-  //   if (!token) {
-  //     throw new UnauthorizedException('Invalid refresh token');
-  //   }
-  //   const user = await this.authService.validateUser(token.userId);
-  //   if (!user) {
-  //     throw new UnauthorizedException('Invalid user');
-  //   }
-  //   const accessToken = await this.authService.generateAccessToken(user);
-  //   const newRefreshToken = await this.authService.generateRefreshToken(user);
-  //   const newTokenData = { accessToken, refreshToken: newRefreshToken };
-  //   return await this.saveToken(newTokenData, user);
-  // }
-
-  // @Post('/get')
-  // async getToken(@Body() user: User): Promise<Token> {
-  //   const existingToken = await this.prisma.token.findUnique({
-  //     where: { userId: user.id },
-  //   });
-
-  //   if (existingToken) {
-  //     return existingToken;
-  //   } else {
-  //     const accessToken = await this.authService.generateAccessToken(user);
-  //     const refreshToken = await this.authService.generateRefreshToken(user);
-  //     const tokenData = { accessToken, refreshToken };
-  //     return await this.saveToken(tokenData, user);
-  //   }
-  // }
+  @Post()
+  async create(@Body() data: { email: string, hash: string}) {
+    const token = await this.prisma.getClient().token.create({
+      data: {
+        email: data.email,
+        hash: bcrypt.hashSync(data.hash, 8),
+      },
+    });
+    return token;
+  }
 
 }
