17277b8ac69848464d35d6f02ac9c094848a12fd
Thalis Mateus Barcellos Macedo <100248956+Alcaanon@users.noreply.github.com>
2023-03-02 22:40:50 -0300

    UPDATE

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -7,21 +7,6 @@ export class TokenController {
     private readonly prisma: PrismaService,
   ) {}
 
-  // @Post()
-  // async save(@Body() hash: string, email: string) {
-  //   let objToken = await this.prisma.getClient().token.findUnique({ where: { email }  });
-  //   if (objToken){
-  //     this.prisma.getClient().token.update({
-  //       where: {email},
-  //       data: { hash }
-  //     });
-  //   }else{
-  //     this.prisma.getClient().token.create({
-  //       data: { email, hash }
-  //     })
-  //   }
-  // }
-
   @Post()
   async save(@Body() hash: string, email: string) {
     const upsertUser = await this.prisma.getClient().token.upsert({
