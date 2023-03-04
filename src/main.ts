9a9c0e6d3021a23ce0e5c27cce60cd26e0a48129
Thalis Mateus Barcellos Macedo <100248956+Alcaanon@users.noreply.github.com>
2023-03-03 21:19:56 -0300

    UPDATE CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(cors());
+  
+  app.use(cors({
+    origin: 'http://localhost:8100' //url do seu app ionic
+  }));
+
   await app.listen(3000);
 }
 bootstrap();
