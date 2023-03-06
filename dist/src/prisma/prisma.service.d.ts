1177467f46bd1430edc6ef6e8843ae05846ecdb0
Thalis Mateus Barcellos Macedo <100248956+Alcaanon@users.noreply.github.com>
2023-03-05 21:32:22 -0300

    Delete DIST

--- dist/src/prisma/prisma.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-export declare class PrismaService {
-    private client;
-    constructor();
-    getClient(): PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import(".prisma/client").Prisma.RejectOnNotFound | import(".prisma/client").Prisma.RejectPerOperation>;
-}
