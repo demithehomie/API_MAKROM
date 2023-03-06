862c014855eb525a9dc65015ce36906783dbca4f
Thalis Mateus Barcellos Macedo <100248956+Alcaanon@users.noreply.github.com>
2023-03-05 21:32:59 -0300

    Dist UPDATE RAILWAY

---
+++ dist/src/linkdepagamento/pagament.controller.d.ts
@@ -0,0 +1,10 @@
+import { HttpService } from '@nestjs/axios';
+export declare class PagamentoController {
+    private httpService;
+    private readonly asaasApiUrl;
+    constructor(httpService: HttpService);
+    createSubscription(datacliente: any): Promise<any>;
+    getSubscription(): Promise<any>;
+    deleteSubscription(id: string): Promise<any>;
+    updateSubscription(id: string, datacliente: any): Promise<any>;
+}
